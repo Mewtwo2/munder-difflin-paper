001850d1dc8a331dc5db6f3d9c6fdfd2b951e26c
Ibteda Sabur <akkount35@gmail.com>
2018-07-11 19:22:17 -0400

    cant get transitions to work, maybe try classlist add/remove or window.onload

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 var solutionsImage = document.getElementById('solutions-background-image');
 solutionsImage.src = "https://tinyurl.com/y9o3e44p";
+
+
+var hiddenElements = document.getElementsByClassName('hidden');
+console.log(hiddenElements[0])
+
+
+hiddenElements[0].style.opacity = 1;
