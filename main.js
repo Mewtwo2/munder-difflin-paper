65f469c480e3b3d6526cc64fd5b27b311c24dd16
Ibteda Sabur <akkount35@gmail.com>
2018-07-12 10:49:39 -0400

    fixed the inital section transition by adding a window.onload function

--- main.js
+++ main.js
@@ -5,5 +5,7 @@ solutionsImage.src = "https://tinyurl.com/y9o3e44p";
 var hiddenElements = document.getElementsByClassName('hidden');
 console.log(hiddenElements[0])
 
-
-hiddenElements[0].style.opacity = 1;
+window.onload = function() {
+  hiddenElements[0].style.opacity = 1;
+  hiddenElements[1].style.opacity = 1;
+};
