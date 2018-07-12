d0b29f65af2e8b4ec03b1b3b8e8dace478b83e22
Ibteda Sabur <akkount35@gmail.com>
2018-07-12 17:32:58 -0400

    final version ready for grading(more or less)

--- main.js
+++ main.js
@@ -1,11 +1,30 @@
 var solutionsImage = document.getElementById('solutions-background-image');
 solutionsImage.src = "https://tinyurl.com/y9o3e44p";
 
-
 var hiddenElements = document.getElementsByClassName('hidden');
-console.log(hiddenElements[0])
 
 window.onload = function() {
-  hiddenElements[0].style.opacity = 1;
-  hiddenElements[1].style.opacity = 1;
+
+  try {
+    for (obj in hiddenElements) {
+      hiddenElements[obj].style.opacity = 1;
+      if (obj == 1) break;
+    }
+  } catch (err) {}
+
 };
+
+var learnMoreButton = document.getElementById('learn-more-anchor');
+
+learnMoreButton.addEventListener('mouseover', function() {
+  learnMoreButton.style.fontSize = '125%';
+  learnMoreButton.style.cursor = 'pointer';
+  learnMoreButton.style.left = '530px';
+  learnMoreButton.style.color = '#e8f713';
+});
+
+learnMoreButton.addEventListener('mouseout', function() {
+  learnMoreButton.style.fontSize = '100%';
+  learnMoreButton.style.left = '540px';
+  learnMoreButton.style.color = 'white';
+});
